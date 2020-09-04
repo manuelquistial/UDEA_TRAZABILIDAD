7b4b34bbf2a86ae435365e4b317dc4d0b047757a
Manuel Quistial <manuel.quistial@digitalamericas.ai>
2020-09-04 18:42:01 -0500

    View "Centro Costos"

---
+++ resources/views/configuration/centroCostosView.blade.php
@@ -0,0 +1,55 @@
+@extends('layouts.lateralMenu.configuracionView')
+
+@section('content')
+<?php
+    $titulo = e(Lang::get('strings.configuracion.modal.'.$opcion.'.titulo'));
+    $buscar = e(Lang::get('strings.configuracion.modal.'.$opcion.'.placeholder'));
+    $deshabilitar = e(Lang::get('strings.configuracion.modal.'.$opcion.'.deshabilitar'));
+    $escribir_item = e(Lang::get('strings.configuracion.modal.'.$opcion.'.escribir_item'));
+?>
+<div class="modal-header">
+    <div class="col-6 conf-header">
+        <h5>{{ Lang::get('strings.configuracion.centro_costos') }}</h5>
+    </div>
+    <div class="col-6 text-right conf-header">
+        <a class="info-header" href="#" id="nuevo_item">{{ Lang::get('strings.configuracion.nuevo_centro_costo') }}</a>
+    </div>
+</div>
+<div class="card-body">
+    <div class="form-group">
+        <input type="text" class="form-control" id="buscar_item" placeholder="{{ $buscar }}">
+    </div>
+    <div class="table-responsive-sm">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th id="one_column">{{ Lang::get('strings.configuracion.centro_costos') }}</th>
+                    <th>{{ Lang::get('strings.configuracion.habilitado') }}</th>
+                </tr>
+            </thead>
+            <tbody id="items_tabla">
+                @foreach($centro_costos as $centro_costo)
+                <tr>
+                    <td><a href="#{{ $centro_costo->id }}">{{ $centro_costo->centro_costo }}</a></td>
+                    <td>
+                        <label class="switch">
+                            <input type="checkbox" name="habilitar" value="{{ $centro_costo->id }}" {{ $centro_costo->estado_id == 4 ? 'selected' : '' }}>
+                            <span class="slider round"></span>
+                        </label>
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        <nav aria-label="Page navigation tipo transaccion" id="paginacion">
+            {{ $centro_costos->links() }}
+        </nav>
+    </div>
+</div>
+@stop
+
+@section('scripts')
+    @if ($general == 2)
+        <script type="module" src="{{ asset('js/configuracion.js') }}" crossorigin="anonymous"></script>
+    @endif
+@stop
