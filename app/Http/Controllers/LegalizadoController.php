69d59e337afd9c017db06abe517a558ce3abffb8
Manuel Quistial <manuel.quistial@digitalamericas.ai>
2020-09-04 18:43:48 -0500

    Controller "Legalizado"

---
+++ app/Http/Controllers/LegalizadoController.php
@@ -0,0 +1,140 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
+use App\Legalizado;
+
+class LegalizadoController extends Controller
+{
+    public $etapa_id = 9;
+    public $estado_id = 2;
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+    }
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\Legalizado
+     */
+    public function create(array $data)
+    {
+        $legalizado = Legalizado::create([
+            'consecutivo_id' => $data['consecutivo'],
+            'encargado_id' => 1113533874,
+            'reintegro' => $data['reintegro'],
+            'etapa_id' => $this->etapa_id,
+            'fecha_estado' => date("Y-m-d H:i:s")
+        ]);
+
+        return $legalizado;
+    }
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'reintegro' => 'integer'
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $this->validator($request->all())->validate();
+
+        $legalizado = $this->create($request->all());
+        $legalizado->save();
+        $status = DB::table('tr_status')
+                        ->where('consecutivo_id', $request->consecutivo)
+                        ->update(['Legalizado' => True]);
+        return response()->json([''=>$status]);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $consecutivo
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($consecutivo)
+    {
+        $etapas = false;
+        $etapa_id = $this->etapa_id;
+        $etapa_estado;
+
+        try {
+            $etapa_estado = DB::table('tr_etapas AS a')
+                        ->leftJoin('tr_consecutivo_etapa_estado AS b', 'b.etapa_id', '=', 'a.etapa_id')
+                        ->leftJoin('tr_estados AS c', 'c.estado_id', '=', 'b.estado_id')
+                        ->where('b.consecutivo', $consecutivo)
+                        ->orderBy('a.etapa_id', 'asc')
+                        ->select('a.etapa', 'c.estado_id', 'a.endpoint')
+                        ->get();
+            $queryStatus = "ok";
+        } catch(Exception $e) {
+            $queryStatus = "error";
+        }
+
+        return view('etapas/legalizadoView', compact('etapa_id','consecutivo','etapas','etapa_estado'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
