626b5523c7802dde2f8ad41b4e710489561bffc3
Manuel Quistial <manuel.quistial@digitalamericas.ai>
2020-09-04 21:16:49 -0500

    Kernel

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,5 +57,7 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        'role' => \App\Http\Middleware\CheckRole::class,
+        'etapa' => \App\Http\Middleware\HasEtapa::class,
     ];
 }
